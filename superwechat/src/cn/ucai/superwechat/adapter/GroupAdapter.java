82b71738d49652ed30771e7055b9cde88281dae5
zhangqiang <1126959509@qq.com>
2017-02-17 18:57:00 +0800

    设置群组图标的显示方法

--- superwechat/src/cn/ucai/superwechat/adapter/GroupAdapter.java
+++ superwechat/src/cn/ucai/superwechat/adapter/GroupAdapter.java
@@ -27,6 +27,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hyphenate.chat.EMGroup;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 
 import java.util.List;
 
@@ -112,7 +113,7 @@ public class GroupAdapter extends ArrayAdapter<EMGroup> {
 				convertView = inflater.inflate(R.layout.em_row_group, parent, false);
 			}
 			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position - 3).getGroupName());
-
+			EaseUserUtils.setAppGroupAvatar(getContext(),getItem(position-3).getGroupId(), (ImageView) convertView.findViewById(R.id.avatar));
 		}
 
 		return convertView;
