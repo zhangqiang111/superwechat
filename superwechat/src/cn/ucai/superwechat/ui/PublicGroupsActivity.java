49de17d38dcdb9b05f5b2c5ae812dc056cf117c1
zhangqiang <1126959509@qq.com>
2017-02-20 16:55:44 +0800

    公开群聊显示群头像,搜索群显示群头像

--- superwechat/src/cn/ucai/superwechat/ui/PublicGroupsActivity.java
+++ superwechat/src/cn/ucai/superwechat/ui/PublicGroupsActivity.java
@@ -26,6 +26,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -35,6 +36,7 @@ import android.widget.Toast;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMCursorResult;
 import com.hyphenate.chat.EMGroupInfo;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.exceptions.HyphenateException;
 
 import java.util.ArrayList;
@@ -181,6 +183,7 @@ public class PublicGroupsActivity extends BaseActivity {
 			}
 
 			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position).getGroupName());
+            EaseUserUtils.setAppGroupAvatar(getContext(),getItem(position).getGroupId(),((ImageView)convertView.findViewById(R.id.avatar)));
 
 			return convertView;
 		}
